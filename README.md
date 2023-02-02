78a2553590a9e2ef3ca12ebae81ad3304213f3e1
Dinika Saxena <dinikasaxenas@gmail.com>
2023-02-02 18:39:35 +0100

    Remove axios

---
+++ README.md
@@ -0,0 +1,7 @@
+## Simple TypeScript project that uses Musquette as a dependency
+
+Run the following command to start the project
+
+```
+yarn serve
+```
--- app/js/app.ts
+++ app/js/app.ts
@@ -1,11 +1,6 @@
-import axios from 'axios';
 import { MQTTSubject } from 'musquette'
 
 const greeting: string = 'Hello World!';
 alert(greeting);
 
-const res = axios.get<boolean>('')
-
-res.then(body => console.log(body))
-
-const m = new MQTTSubject('') 
+const m = new MQTTSubject('')
